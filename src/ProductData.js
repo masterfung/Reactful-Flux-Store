918d6e790f24f5baab7008e157daa0b8f9f957a0
Tsung Hung <hungmasterj@gmail.com>
2015-05-09 11:44:22 -0700

    product api includes mock photos

--- src/ProductData.js
+++ src/ProductData.js
@@ -6,18 +6,20 @@ export default {
 			{
 				id: '777900',
 				name: 'Signature Fleact Gem - Master Cut Series',
-				image: 'fleact-gem.png',
 				description: 'The finest gem speciment you will find in all of the Fleact Realm. Guaranteed to hold its value and luster. Master cut and perfect. Value quality rating of 9.9997!',
 				variants: [
 					{
 						sku: '901',
 						type: 'Sinki',
+						image: './src/assets/Sinki.png',
+
 						price: 3,
 						inventory: 1590
 
 					},{
 						sku: '909',
 						type: 'Ankro',
+						image: './src/assets/Ankro.png',
 						price: 12.1,
 						inventory: 150
 
@@ -25,18 +27,21 @@ export default {
 					{
 						sku: '912',
 						type: 'Veltra',
+						image: './src/assets/Veltra.png',
 						price: 120,
 						inventory: 10
 					},
 					{
 						sku: '975',
 						type: 'Mezinato',
+						image: './src/assets/Mezinato.png',
 						price: 6555,
 						inventory: 3
 					},
 					{
 						sku: '999',
 						type: 'Perfectina',
+						image: './src/assets/Perfectina.png',
 						price: 9999,
 						inventory: 2
 					}
